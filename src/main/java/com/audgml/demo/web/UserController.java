6bf863ebe9c3e506cdd39e84d4fd8bebbde821f5
anstnsp <anstnsp@naver.com>
2020-07-04 20:03:23 +0900

    oauth 하는중

---
+++ src/main/java/com/audgml/demo/web/UserController.java
@@ -0,0 +1,24 @@
+package com.audgml.demo.web;
+
+import com.audgml.demo.domain.user.UserRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+  
+  private UserRepository userRepository; 
+
+  @GetMapping("/user/me")
+  @PreAuthorize("hasRole('USER')") // @PreAuthorize 먼지 찾아보기 
+  public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
+    return userRepository.findById(userPrincipal.getId())
+    .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
+  }
+  
+
+}
